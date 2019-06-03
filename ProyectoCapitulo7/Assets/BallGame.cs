30710df7212c84a61b34f0ceaf6bab35a7333c52
yonathan <yhona777@gmail.com>
2019-06-03 14:38:28 -0500

    Agregando Capitulo7

---
+++ ProyectoCapitulo7/Assets/BallGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGame : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+
+	public GameObject ball;
+	public float startHeight = 10f;
+	public float fireInterval = 5f;
+
+	private float nextBallTime = 0.0f;
+	private GameObject activeBall;
+	private Transform head;
+	private AudioSource audio;
+
+	void Start () {
+		head = Camera.main.transform;
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void Update () {
+		if (Time.time > nextBallTime) {
+			nextBallTime = Time.time + fireInterval;
+			audio.Play ();
+			Vector3 position = new Vector3( head.position.x,
+				startHeight, head.position.z + 0.2f );
+			activeBall = Instantiate( ball, position,
+				Quaternion.identity ) as GameObject;
+		}
+	}
+}
---
+++ ProyectoCapitulo7/Assets/ButtonExecuteTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class ButtonExecuteTest : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+
+
+	private GameObject startButton, stopButton;
+	private bool on = false;
+	private float timer = 5.0f;
+	void Start () {
+		startButton = GameObject.Find ("StartButton");
+		stopButton = GameObject.Find ("StopButton");
+	}
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0.0f) {
+			on = !on;
+			timer = 5.0f;
+			PointerEventData data = new PointerEventData
+				(EventSystem.current);
+			if (on) {
+				ExecuteEvents.Execute<IPointerClickHandler> (startButton,
+					data, ExecuteEvents.pointerClickHandler);
+			} else {
+				ExecuteEvents.Execute<IPointerClickHandler> (stopButton,
+					data, ExecuteEvents.pointerClickHandler);
+			}
+		}
+	}
+
+}
